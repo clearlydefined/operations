ebd465844b7f5fb5eb369959167d4dbcd10109ea
yashkohli88 <yash.kohli@sap.com>
2024-07-01 10:40:05 +0530

    Fixed formatting

--- tools/integration/test/integration/e2e-test-service/noticeTest.js
+++ tools/integration/test/integration/e2e-test-service/noticeTest.js
@@ -8,47 +8,52 @@ const nock = require('nock')
 const fs = require('fs')
 
 describe('Validate notice files between dev and prod', function () {
-    this.timeout(definition.timeout)
+  this.timeout(definition.timeout)
 
-    //Rest a bit to avoid overloading the servers
-    afterEach(() => new Promise(resolve => setTimeout(resolve, definition.timeout / 2)))
+  //Rest a bit to avoid overloading the servers
+  afterEach(() => new Promise(resolve => setTimeout(resolve, definition.timeout / 2)))
 
-    before(() => {
-        loadFixtures().forEach(([coordinatesString, notice]) => {
-            nock(prodApiBaseUrl, { allowUnmocked: true }).post('/notices', { "coordinates": [coordinatesString] }).reply(200, notice)
-        })
+  before(() => {
+    loadFixtures().forEach(([coordinatesString, notice]) => {
+      nock(prodApiBaseUrl, { allowUnmocked: true })
+        .post('/notices', { coordinates: [coordinatesString] })
+        .reply(200, notice)
     })
+  })
 
-    components.forEach(coordinates => {
-        it(`should return the same notice as prod for ${coordinates}`, () => fetchAndCompareNotices(coordinates))
-    })
+  components.forEach(coordinates => {
+    it(`should return the same notice as prod for ${coordinates}`, () => fetchAndCompareNotices(coordinates))
+  })
 })
 
 async function fetchAndCompareNotices(coordinates) {
-    const [computedNotice, expectedNotice] = await Promise.all(
-        [
-            callFetch(`${devApiBaseUrl}/notices`, buildPostOpts({
-                "coordinates": [
-                    coordinates
-                ]
-            })),
-            callFetch(`${prodApiBaseUrl}/notices`, buildPostOpts({
-                "coordinates": [
-                    coordinates
-                ]
-            }))
-        ].map(p => p.then(r => r.json())))
-    deepStrictEqual(computedNotice, expectedNotice)
+  const [computedNotice, expectedNotice] = await Promise.all(
+    [
+      callFetch(
+        `${devApiBaseUrl}/notices`,
+        buildPostOpts({
+          coordinates: [coordinates]
+        })
+      ),
+      callFetch(
+        `${prodApiBaseUrl}/notices`,
+        buildPostOpts({
+          coordinates: [coordinates]
+        })
+      )
+    ].map(p => p.then(r => r.json()))
+  )
+  deepStrictEqual(computedNotice, expectedNotice)
 }
 
 function loadFixtures() {
-    const location = 'test/integration/fixtures/notices'
-    return fs
-        .readdirSync(location)
-        .filter(f => f.endsWith('.json'))
-        .map(jsonFile => {
-            const notice = JSON.parse(fs.readFileSync(`${location}/${jsonFile}`))
-            const coordinatesString = jsonFile.replaceAll('-', '/').replaceAll('///', '/-/').replace('.json', '')
-            return [coordinatesString, notice]
-        })
+  const location = 'test/integration/fixtures/notices'
+  return fs
+    .readdirSync(location)
+    .filter(f => f.endsWith('.json'))
+    .map(jsonFile => {
+      const notice = JSON.parse(fs.readFileSync(`${location}/${jsonFile}`))
+      const coordinatesString = jsonFile.replaceAll('-', '/').replaceAll('///', '/-/').replace('.json', '')
+      return [coordinatesString, notice]
+    })
 }
