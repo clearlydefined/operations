eb8bfee8f73a8a49c8da926d694455a753b2a407
Lewis Jones <ljones140@gmail.com>
2024-09-09 15:08:51 +0100

    Just have 2 for quick testing

--- tools/integration/test/integration/testConfig.js
+++ tools/integration/test/integration/testConfig.js
@@ -12,23 +12,23 @@ const harvestTools = ['licensee', 'reuse', 'scancode']
 
 //Components to test
 const components = [
-  'pypi/pypi/-/platformdirs/4.2.0', //Keep this as the first element to test, it is relatively small
+  // 'pypi/pypi/-/platformdirs/4.2.0', //Keep this as the first element to test, it is relatively small
   'maven/mavencentral/org.apache.httpcomponents/httpcore/4.4.16',
-  'maven/mavengoogle/android.arch.lifecycle/common/1.0.1',
-  'maven/gradleplugin/io.github.lognet/grpc-spring-boot-starter-gradle-plugin/4.6.0',
+  // 'maven/mavengoogle/android.arch.lifecycle/common/1.0.1',
+  // 'maven/gradleplugin/io.github.lognet/grpc-spring-boot-starter-gradle-plugin/4.6.0',
   // 'conda/conda-forge/linux-aarch64/numpy/1.16.6-py36hdc1b780_0',
-  'crate/cratesio/-/ratatui/0.26.0',
-  'npm/npmjs/-/redis/0.1.0',
-  'git/github/ratatui-org/ratatui/bcf43688ec4a13825307aef88f3cdcd007b32641',
-  'gem/rubygems/-/sorbet/0.5.11226',
-  'pypi/pypi/-/sdbus/0.12.0',
+  // 'crate/cratesio/-/ratatui/0.26.0',
+  // 'npm/npmjs/-/redis/0.1.0',
+  // 'git/github/ratatui-org/ratatui/bcf43688ec4a13825307aef88f3cdcd007b32641',
+  // 'gem/rubygems/-/sorbet/0.5.11226',
+  // 'pypi/pypi/-/sdbus/0.12.0',
   'go/golang/rsc.io/quote/v1.3.0',
-  'nuget/nuget/-/NuGet.Protocol/6.7.1',
-  'composer/packagist/symfony/polyfill-mbstring/v1.28.0',
-  'pod/cocoapods/-/SoftButton/0.1.0', // Dev and prod have different file counts. See https://github.com/clearlydefined/crawler/issues/529
-  'deb/debian/-/mini-httpd/1.30-0.2_arm64',
-  'debsrc/debian/-/mini-httpd/1.30-0.2',
-  'pod/cocoapods/-/xcbeautify/0.9.1'
+  // 'nuget/nuget/-/NuGet.Protocol/6.7.1',
+  // 'composer/packagist/symfony/polyfill-mbstring/v1.28.0',
+  // 'pod/cocoapods/-/SoftButton/0.1.0', // Dev and prod have different file counts. See https://github.com/clearlydefined/crawler/issues/529
+  // 'deb/debian/-/mini-httpd/1.30-0.2_arm64',
+  // 'debsrc/debian/-/mini-httpd/1.30-0.2',
+  // 'pod/cocoapods/-/xcbeautify/0.9.1'
   // 'sourcearchive/mavencentral/org.apache.httpcomponents/httpcore/4.1' // Dev and prod have different license and scores. See https://github.com/clearlydefined/crawler/issues/533
 ]
 
