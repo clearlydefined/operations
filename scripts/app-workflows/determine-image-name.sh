15aec70056aadf18b414aa68b98011298f240809
Lewis Jones <ljones140@github.com>
2024-10-04 17:28:27 +0100

    Deploy image to docker optionally, as well as GHCR (#97)
    To migrate deployment of Production Crawler to CD Azure from Microsoft owned Azure.
    Changes to workflow app-build-and-deploy
    
    Enables the optional docker image publish to Docker Hub, conditional on providing
    
    Input docker-hub-username
    secret DOCKERHUB_TOKEN
    Without these the action only deploys to ghcr
    
    ---------
    
    Co-authored-by: E. Lynette Rayle <elrayle@users.noreply.github.com>

--- scripts/app-workflows/determine-image-name.sh
+++ scripts/app-workflows/determine-image-name.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Inputs
-#   $1 - repo: the orgname/reponame where the image will be published (e.g. 'clearlydefined/service')
+#   $1 - repo_name: the reponame where the image will be published (e.g. 'service')
 #   $2 - deploy_env: environment to deploy (i.e. dev | prod) - used as a label for the Docker image
 #   $3 - image-tag: the tag to use for the image (e.g. prod: v1.2.0, dev: v1.2.0+dev:1D3F567890)
 #
 # Outputs
-#   image_name_with_tag: the full image name with tag (e.g. ghcr.io/clearlydefined/service:v1.2.0)
+#   image_name_with_tag: the full image name with tag (e.g. service:v1.2.0, service-dev:v1.2.0+dev:1D3F567890)
 
-repo="$1"
+repo_name="$1"
 deploy_env="$2"
 image_tag="$3"
 
-image_base_name="ghcr.io/$repo" # e.g. ghcr.io/clearlydefined/service
+image_name_with_tag=""
 if [[ "$deploy_env" == 'prod' ]] ; then
-    image_name_with_tag="$image_base_name:$image_tag"
+    image_name_with_tag="$repo_name:$image_tag"
 elif [[ "$deploy_env" == 'dev' ]] ; then
-    image_name_with_tag="$image_base_name-dev:$image_tag"
+    image_name_with_tag="$repo_name-dev:$image_tag"
 else
     echo "ERROR: Invalid deploy environment: $deploy_env. Must be 'dev' or 'prod'"
     exit 1
