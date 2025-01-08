a99f7cde26d17ae2e1c19dc12f28ae3da6fb5a85
yashkohli88 <yash.kohli@sap.com>
2025-01-08 20:46:58 +0530

    Update copyright header

--- tools/integration/test/integration/e2e-test-service/originsTest.js
+++ tools/integration/test/integration/e2e-test-service/originsTest.js
@@ -1,3 +1,6 @@
+// (c) Copyright 2024, SAP SE and ClearlyDefined contributors. Licensed under the MIT license.
+// SPDX-License-Identifier: MIT
+
 const assert = require('assert')
 const { callFetch } = require('../../../lib/fetch')
 const { devApiBaseUrl, prodApiBaseUrl, getComponents, definition } = require('../testConfig')
